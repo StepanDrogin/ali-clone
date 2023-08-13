17617b3bcd21961d0ddcab9d2a2e583a78e1d7ac
Stepan <s.drogin@think24.ru>
2023-08-13 20:25:15 +0400

    added product component

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
     <MainLayout>
         <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            IndexPage
+            <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+                <div v-if="products" v-for="product in products" :key="product">
+                    <ProductComponent :product="product"/>
+                </div>
+            </div>
         </div>
     </MainLayout>
 </template>
 
 <script setup>
-import MainLayout from '~/layouts/MainLayout.vue'; 
+import MainLayout from '~/layouts/MainLayout.vue';
+
+const products = [
+    {id: 1, title: 'Title 1', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 2, title: 'Title 2', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 3, title: 'Title 3', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 4, title: 'Title 4', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 5, title: 'Title 5', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 6, title: 'Title 6', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 7, title: 'Title 7', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 8, title: 'Title 8', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 9, title: 'Title 9', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+    {id: 10, title: 'Title 10', description: 'This is description', url: 'https://as2.ftcdn.net/v2/jpg/00/97/58/97/1000_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg', price: 9999},
+ 
+]
 </script>
