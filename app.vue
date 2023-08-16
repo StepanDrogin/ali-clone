85cac4f6084881cfa8ab35968a1871e2ccdade6b
Stepan <s.drogin@think24.ru>
2023-08-16 22:00:46 +0400

    changed isloading

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="fixed z-[-1] bg-[#F2F2F2] w-full h-[100vh]"></div>
-  <NuxtPage/>
+  <div class="fixed z-[-1] bg-[#F2F2F2] w-full h-[100vh]"/>
+  <NuxtPage />
+
   <MenuOverlay
     :class="[
         {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
@@ -18,7 +19,7 @@ const route = useRoute()
 let windowWidth = ref(process.client ? window.innerWidth : '')
 
 onMounted(() => {
-    userStore.isLoading = false
+    userStore.isLoading = true
     window.addEventListener('resize', function () {
         windowWidth.value = window.innerWidth;
     });
